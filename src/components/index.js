ca611034660361a7de64b8e7e99ea0e89e77606f
Miisa Suorsa <miisasuorsa@gmail.com>
2023-08-08 19:31:37 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.js 	new file:   src/components/Feed.jsx 	new file:   src/components/MovieDetail.jsx 	new file:   src/components/Navbar.jsx 	new file:   src/components/SearchFeed.jsx 	new file:   src/components/index.js 	modified:   src/index.css 	modified:   src/index.js

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar';
+export { default as Feed } from './Feed';
+export { default as MovieDetail } from './MovieDetail';
+export { default as SearchFeed } from './SearchFeed';
